73850c9a721c690d29df77c1ec7ddaa11056c73b
Andrey Voronkov <andrey.voronkov@medm.com>
2015-09-15 14:26:59 +0300

    0.1.3 -> 0.2.0. Error handling improved.

--- lib/apns_simple/client.rb
+++ lib/apns_simple/client.rb
@@ -6,6 +6,21 @@ module ApnsSimple
 
     attr_reader :certificate, :passphrase, :host, :port
 
+    COMMAND = 8
+    CODES = {
+      0 => 'No errors encountered',
+      1 => 'Processing error',
+      2 => 'Missing device token',
+      3 => 'Missing topic',
+      4 => 'Missing payload',
+      5 => 'Invalid token size',
+      6 => 'Invalid topic size',
+      7 => 'Invalid payload size',
+      8 => 'Invalid token',
+      10 => 'Shutdown',
+      255 => 'Unknown error'
+    }
+
     def initialize(options)
       @certificate = options.fetch(:certificate)
       @passphrase = options[:passphrase] || ''
@@ -26,8 +41,8 @@ module ApnsSimple
         ssl.flush
 
         if IO.select([ssl], nil, nil, 1) && error = ssl.read(6)
-          _command, status, _index = error.unpack("ccN")
-          notification.error = status
+          command, status, _index = error.unpack("ccN")
+          notification.error = command == COMMAND ? "#{status}: #{CODES[status]}" : "Unknown command received from APNS server: #{command}"
         end
       ensure
         ssl.close if ssl
