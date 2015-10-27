e264b28f997fa638cd66bb2a807016c01a8b0de9
Victor Polozov <victor.polozov@medm.com>
2015-10-27 19:39:37 +0300

    Custom payload support

--- lib/apns_simple/notification.rb
+++ lib/apns_simple/notification.rb
@@ -3,15 +3,16 @@ require 'json'
 module ApnsSimple
   class Notification
 
-    attr_reader :token, :alert, :badge, :sound, :content_available
+    attr_reader :token, :alert, :badge, :sound, :content_available, :custom_payload
     attr_accessor :error
 
-    def initialize(options)
+    def initialize(options, custom_payload = {})
       @token = options.fetch(:token)
       @alert = options[:alert]
       @badge = options[:badge]
       @sound = options[:sound] || 'default'
       @content_available = options[:content_available]
+      @custom_payload = custom_payload
     end
 
     def payload
@@ -20,6 +21,7 @@ module ApnsSimple
       payload[:aps][:badge] = badge if badge
       payload[:aps][:sound] = sound if sound
       payload[:aps]['content-available'] = 1 if content_available
+      payload.merge! custom_payload
 
       packed_message = payload.to_json.gsub(/\\u([\da-fA-F]{4})/) {|m| [$1].pack("H*").unpack("n*").pack("U*")}
       packed_token = [token.gsub(/[\s|<|>]/,'')].pack('H*')
